c6f294ddd628835aeffd3d7bff7f24de652a348f
oluseye00 <joseowolabi3@gmail.com>
2023-05-25 01:53:28 -0400

    created vpc file

--- variables.tf
+++ variables.tf
@@ -13,3 +13,39 @@ variable "environment" {
   description = "environment"
   type        = string
 }
+
+#vpc variables
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type        = string
+}
+
+variable "public_subnet_az1_cidr" {
+  description = "public sn az1 cidr block"
+  type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "public sn az2 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+  description = "private app sn az1 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  description = "private app sn az2 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  description = "private data sn az1 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  description = "private data sn az2 cidr block"
+  type        = string
+}
