bf0d3b98f2297b7c6cb710c1e0b20a253b2624a1
oluseye00 <joseowolabi3@gmail.com>
2023-05-26 09:46:11 -0400

    created new files

--- variables.tf
+++ variables.tf
@@ -49,3 +49,41 @@ variable "private_data_subnet_az2_cidr" {
   description = "private data sn az2 cidr block"
   type        = string
 }
+
+#security-group variables
+variable "ssh_location" {
+  description = "ip address that can ssh into the servers"
+  type        = string
+}
+
+# rds variables
+variable "database_snapshot_identifier" {
+  description = "database snapshot name"
+  type        = string
+}
+
+variable "database_instance_class" {
+  description = "database instance class"
+  type        = string
+}
+
+variable "database_instance_identifier" {
+  description = "database instance identifier"
+  type        = string
+}
+
+variable "multi_az_deployment" {
+  description = "create a standby db instance"
+  type        = bool
+}
+
+#acm variables
+variable "domain_name" {
+  description = "domain name"
+  type        = string
+}
+
+variable "alternative_names" {
+  description = "sub domain name"
+  type        = string
+}
